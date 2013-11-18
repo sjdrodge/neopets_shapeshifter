dc79d445ba05439f889882301a06f9dcee14b169
Stephen Drodge <stephen.drodge@gmail.com>
2013-11-17 20:52:40 -0500

    Substantial rewrite: fixed idiotic bug that had gone unnoticed for several versions, rewrote inner loop w/ ST, added strictness annotations, and made some random style changes.

--- ShapeShifter.hs
+++ ShapeShifter.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
 module ShapeShifter ( GameBoard
                     , GameShape
-                    , GameState(..)
+                    , GameState (..)
                     , GamePlan
                     , ppGameBoard
                     , ppGamePlan
@@ -11,15 +11,19 @@ module ShapeShifter ( GameBoard
                     , checksum
                     ) where
 
+import Control.DeepSeq (($!!), NFData)
 import Control.Monad
+import Control.Monad.ST
 import Control.Monad.State
 import Data.Aeson.Types (FromJSON (parseJSON), genericParseJSON, defaultOptions, fieldLabelModifier)
+import Data.Int (Int8)
 import Data.List
 import Data.Ord
-import Data.Int (Int8)
+import Data.STRef
 import GHC.Generics (Generic)
 import Text.Regex (mkRegex, subRegex)
 import qualified Data.Vector.Unboxed as U
+import qualified Data.Vector.Unboxed.Mutable as UM
 
 type BoardIndex = (Int, Int)
 
@@ -30,19 +34,19 @@ type Modularity = Int8
 type BoardEntry = Modularity
 
 data GameBoard = GameBoard { dimensions :: BoardIndex
-                           , boardData :: U.Vector BoardEntry
+                           , boardData  :: U.Vector BoardEntry
                            } deriving (Generic, Show)
 
 type GameShape = GameBoard
 
 data GameState = GameState { modularity :: Modularity
-                           , board :: GameBoard
-                           , shapes :: [GameShape]
+                           , board      :: GameBoard
+                           , shapes     :: [GameShape]
                            } deriving (Generic, Show)
 
 type GamePlan = [(GameShape, BoardIndex)]
 
-type Delta = U.Vector (BoardOffset, BoardEntry)
+type Delta = U.Vector BoardOffset
 
 data GameShape_ = GameShape_ { sDims   :: BoardIndex
                              , sMass   :: Int
@@ -56,6 +60,9 @@ data GameBoard_ = GameBoard_ { bDims :: BoardIndex
                              }
 
 type GamePlan_ = [(GameShape_, BoardIndex)]
+
+instance NFData GameBoard_
+instance NFData GameShape_
 
 instance FromJSON GameBoard where
     parseJSON = genericParseJSON defaultOptions
@@ -82,15 +89,15 @@ rcIndex :: GameBoard -> Int -> Int -> BoardEntry
 rcIndex b r c = boardData b U.! (numCols b * r + c)
 
 ppGameBoard :: GameBoard -> String
-ppGameBoard b = unlines ("":[ unwords [ show ( rcIndex b i j )
-                                | j <- [0 .. (numCols b - 1)]
-                                ]
-                                | i <- [0 .. (numRows b - 1)]
-                            ])
+ppGameBoard b = unlines ("" : [ unwords [ show ( rcIndex b i j )
+                                  | j <- [0 .. (numCols b - 1)]
+                                  ]
+                                  | i <- [0 .. (numRows b - 1)]
+                              ])
 
 ppGamePlan :: GamePlan -> String
 ppGamePlan = unwords . map f
-    where f (sh, (n,m)) = ppGameBoard sh ++ "-\n" ++ show (n + 1, m + 1) ++ "\n"
+    where f (sh, (n, m)) = ppGameBoard sh ++ "-\n" ++ show (n + 1, m + 1) ++ "\n"
 
 ppGameState :: GameState -> String
 ppGameState st = ppGameBoard b ++ "% " ++ show (modularity st)
@@ -105,13 +112,13 @@ numCols = snd . dimensions
 boardSize :: GameBoard -> BoardIndex
 boardSize b = (numRows b, numCols b)
 
-shapeIndicesList :: GameBoard -> GameShape -> BoardIndex -> [(BoardOffset, BoardEntry)]
+shapeIndicesList :: GameBoard -> GameShape -> BoardIndex -> [BoardOffset]
 shapeIndicesList b s i = do
     r <- [0 .. (numRows s - 1)]
     c <- [0 .. (numCols s - 1)]
     let (r0, c0) = i
     guard ( rcIndex s r c == 1 )
-    return ( numCols b * (r0 + r) + (c0 + c), 1 )
+    return $!! numCols b * (r0 + r) + (c0 + c)
 
 possibleIndices :: GameBoard -> GameShape -> [BoardIndex]
 possibleIndices b sh = do
@@ -120,17 +127,31 @@ possibleIndices b sh = do
     return (r, c)
 
 applyDelta :: Modularity -> GameBoard_ -> Delta -> GameBoard_
-applyDelta m b d = b { bDist = bDist b + distDelta
-                     , bData = bData'
-                     } where f x y     = (x + y) `rem` m
-                             bData'    = U.accumulate f (bData b) d
-                             distDelta = U.foldr (\(i,_) z -> fromIntegral ((bData' U.! i) - (bData b U.! i)) + z) 0 d
+applyDelta m b d = runST $ do
+    dist' <- newSTRef (bDist b)
+    nref <- newSTRef 0
+    vec' <- U.thaw (bData b)
+
+    U.forM_ d $ \i -> do
+        x <- UM.read vec' i
+        let x' = x + 1
+        unless (x' == m) $ UM.write vec' i x'
+        when (x' == m) $ UM.write vec' i 0
+        when (x' == 1) $ modifySTRef nref (+ 1)
+
+    n <- readSTRef nref
+    modifySTRef dist' (+ (n * fromIntegral m - U.length d))
+
+    dist'' <- readSTRef dist'
+    vec'' <- U.unsafeFreeze vec'
+    return $!! b {bData = vec'', bDist = dist''}
+
 
 possiblePlans :: Modularity -> GameBoard_ -> GameShape_ -> [(BoardIndex, GameBoard_)]
 possiblePlans m b s = do
     (i, d) <- sDeltas s
     let b' = applyDelta m b d
-    return (i, b')
+    return $!! (i, b')
 
 mass :: GameShape -> Int
 mass = U.sum . U.map fromIntegral . boardData
@@ -143,13 +164,13 @@ distanceFromMass :: Modularity -> GameBoard_ -> [GameShape_] -> Int
 distanceFromMass _ b xs = (sum . map sMass ) xs - bDist b
 
 pruneAndSort :: Modularity -> [GameShape_] -> [(BoardIndex, GameBoard_)] -> [(BoardIndex, GameBoard_)]
-pruneAndSort m ss = sortBy (comparing h) . filter ((0<=) . h)
+pruneAndSort m ss = sortBy (comparing h) . filter ((0 <=) . h)
     where h (_, b) = distanceFromMass m b ss
 
 shapeShifter' :: Modularity -> [GameShape_] -> (BoardIndex, GameBoard_) -> Maybe GamePlan_
 shapeShifter' m (s:ss) (i, b) = do
     ret <- shapeShifter m b ss
-    return $ (s, i) : ret
+    return $!! (s, i) : ret
 
 shapeShifter :: Modularity -> GameBoard_ -> [GameShape_] -> Maybe GamePlan_
 shapeShifter _ b ss | null ss = if bDist b == 0 then Just [] else Nothing
